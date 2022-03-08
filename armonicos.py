169f9b4aca3c576a99d761f2a09cde27c34a47cc
Danna-Aguilar <69810489+Danna-Aguilar@users.noreply.github.com>
2022-03-07 19:56:55 -0600

    Add files via upload

--- armonicos.py
+++ armonicos.py
@@ -1,11 +1,10 @@
 from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.io import wavfile
 from scipy.io.wavfile import write
 
 frecuencia_muestreo = 44100
-frecuencia = 250
+frecuencia = 440
 tiempos = np.linspace(0.0, 1.0, frecuencia_muestreo)
 amplitud = np.iinfo(np.int16).max
 
@@ -23,9 +22,11 @@ data = data - data.mean()
 alto, bajo = abs(max(data)), abs(min(data))
 data = amplitud * data / max(alto, bajo)
 
-plt.figure()
-plt.plot(tiempos, data)
-plt.show()
+fig, ejes = plt.subplots(2,4)
+
+# plt.figure()
+ejes[0,0].plot(tiempos, data)
+# plt.show()
 
 write("triangular.wav", frecuencia_muestreo, data.astype(np.int16))
 
@@ -34,65 +35,52 @@ periodo_muestreo = 1.0 / frecuencia_muestreo
 transformada = np.fft.rfft(data)
 frecuencias = np.fft.rfftfreq(cantidad_muestras, periodo_muestreo)
 
-plt.figure()
-plt.plot(frecuencias, np.abs(transformada))
-plt.show()
+# plt.figure()
+ejes[0,1].plot(frecuencias, np.abs(transformada))
+# plt.show()
 
 # 1.- Obtener en Hz las frecuencias de los armoicos de la señal
 
-umbral = 100000
-print(frecuencias[transformada > umbral])
-transformada[transformada > umbral]
+armonicos = frecuencias[transformada > 150000]
+print(armonicos)
 
 # 2.- Aplicarle un filtro pasa bajas que solo deje pasar la Freq Fundamental 
 # y luego aplicarle la transformada inversa
 # graficarla en dominio del tiempo
 # y crear un archivo wav para escucharla
 
-frecuencia_muestreo, muestras = wavfile.read("triangular.wav")
-
-print(frecuencia_muestreo)
-print("Tipo: " + str(type(muestras)))
-print("Dtype (bithdept): " + str(muestras.dtype))
-print("shape = " + str(muestras.shape))
-
-canales = 1
-if len(muestras.shape) == 1:
-    print("# Canales = 1")
-else:
-    print("# Canales = " + str(muestras.shape[1]))
-    canales = muestras.shape[1]
-duracion = muestras.shape[0] / frecuencia_muestreo
-print("duracion:  " + "{:.2f}".format(duracion) + " segs")
-
-figura, ejes = plt.subplots(2,2)
-if canales == 1:
-    ejes[0,0].plot(tiempos, muestras, label="Canal mono")
-else:
-    ejes[0,0].plot(tiempos, muestras[:, 0], label="Izquierdo")
-    ejes[0,0].plot(tiempos, muestras[:, 1], label="Derecho")
-
-ejes[0,0].legend()
-ejes[0,0].set(xlabel = "Tiempo (s)", ylabel = "Amplitud")
-
-if canales > 1:
-    data = muestras[:,0]
-else:
-    data = muestras
-
 pasa_bajas = transformada.copy()
-pasa_bajas[frecuencias > frecuencia] *=0
+pasa_bajas[frecuencias > (frecuencia + 5)] *= 0
 
-ejes[1,1].plot(frecuencias, np.abs(pasa_bajas), 
-    label = "Espectro filtrado, pasa bajas")
-ejes[1,1].legend()
-ejes[1,1].set(xlabel = "Frecuencia (Hz)", ylabel = "Amplitud")
+# plt.figure()
+ejes[0,3].plot(frecuencias, np.abs(pasa_bajas), label = "Pasa bajas")
+ejes[0,3].legend()
+# plt.show
 
 pasa_bajas_data = np.fft.irfft(pasa_bajas)
-ejes[1,0].plot(tiempos, pasa_bajas_data, label = "Audio con pasa bajas")
-ejes[1,1].legend()
-ejes[1,1].set(xlabel = "Tiempo (s)", ylabel = "Amplitud")
+# plt.figure()
+ejes[0,2].plot(tiempos, pasa_bajas_data)
 
 write("pasa_bajas.wav", frecuencia_muestreo, pasa_bajas_data.astype(np.int16))
+
+ciclos_rectangular = frecuencia * tiempos
+fracciones_rectangular, enteros_rectangular = np.modf(ciclos_rectangular)
+data_rectangular = amplitud * np.sign(fracciones_rectangular - fracciones_rectangular.mean())
+
+ejes[1,0].plot(tiempos, data_rectangular)
+
+transformada_rectangular = np.fft.rfft(data_rectangular)
+frecuencias_rectangular = np.fft.rfftfreq(cantidad_muestras, periodo_muestreo)
+
+ejes[1,1].plot(frecuencias_rectangular, np.abs(transformada_rectangular))
+
+pasa_bajas_rectangular = transformada_rectangular.copy()
+pasa_bajas_rectangular[frecuencias_rectangular > (frecuencia + 5)] *= 0
+
+ejes[1,3].plot(frecuencias_rectangular, np.abs(pasa_bajas_rectangular))
+
+pasa_bajas_data_rectangular = np.fft.irfft(pasa_bajas_rectangular)
+
+ejes[1,2].plot(tiempos, pasa_bajas_data_rectangular)
 
 plt.show()
